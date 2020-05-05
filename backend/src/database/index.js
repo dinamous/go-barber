a6fe88a77d0f5cba12f9839e5510a93bd5d3a5a5
Matheus <matheusaugustosimoes@hotmail.com>
2020-05-05 16:33:17 -0300

    controller de usuario

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -1,7 +1,8 @@
 import Sequelize from 'sequelize';
-import databaseConfig from '../config/database';
 
 import User from '../app/models/User';
+
+import databaseConfig from '../config/database';
 
 const models = [User];
 
@@ -11,7 +12,7 @@ class Database {
     }
 
     init() {
-        this.connection = Sequelize(databaseConfig);
+        this.connection = new Sequelize(databaseConfig);
         models.map((model) => model.init(this.connection));
     }
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,9 @@
 import { Router } from 'express';
 
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-    const user = await User.create({
-        name: 'matheus',
-        email: 'math@hotmail.com',
-        password_hash: '123321',
-        provider: false,
-    });
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
