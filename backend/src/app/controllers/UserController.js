992a1c96ce270f58aedd8dac0886218aa7457775
Matheus <matheusaugustosimoes@hotmail.com>
2020-05-07 14:00:16 -0300

    update de usuario feito

--- backend/src/app/controllers/UserController.js
+++ backend/src/app/controllers/UserController.js
@@ -14,6 +14,28 @@ class UserController {
 
         return res.json({ id, name, email, provider });
     }
+
+    async update(req, res) {
+        const { email, oldPassword } = req.body;
+        const user = await User.findByPk(req.userId);
+
+        if (email !== user.email) {
+            const userExist = await User.findOne({
+                where: { email },
+            });
+
+            if (userExist) {
+                return res.status(400).json({ error: 'User already exists' });
+            }
+        }
+
+        if (oldPassword && !(await user.checkPassword(oldPassword))) {
+            return res.status(401).json({ error: 'Password does not match' });
+        }
+
+        const { id, name, provider } = await user.update(req.body);
+        return res.json({ id, name, email, provider });
+    }
 }
 
 export default new UserController();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -2,10 +2,16 @@ import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import authMiddleware from './app/middleware/auth';
 
 const routes = new Router();
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.use(authMiddleware);
+//  todas as rotas apos este middleware precisam que o usuario esteja logado
+
+routes.put('/users', UserController.update);
 
 export default routes;
